588c608f18cb62bedd1dcf0c7f6ec59d2d4dab15
Dylan245 <dpr245@live.missouristate.edu>
2018-01-26 13:10:32 -0600

    Initial import of csc131 package.

---
+++ csc131/Dictionaries.py
@@ -0,0 +1,6 @@
+def get_personal_data() -> dict:
+    """
+    :return: Returns a dictionary of personal information.
+    """
+    personal_data = {"name": "Jim", "a_role": "teacher"}
+    return personal_data
---
+++ lecture2018-26-01.py
@@ -0,0 +1,15 @@
+from csc131 import Dictionaries
+
+
+def main():
+    print("Main")
+    d = Dictionaries.get_personal_data()
+    print(d)
+    for key in sorted(d.keys()):
+        print("%s: %s" % (key, d[key]))
+
+
+
+
+if __name__ == '__main__':
+    main()
